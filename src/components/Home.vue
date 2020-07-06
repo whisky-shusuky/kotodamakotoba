7be46195940c8354e5770528c0cca401ddd671ab
whisky shusuky <whisky.shusuky@gmail.com>
2020-07-06 20:42:26 +0900

    fix(script): cosole出力して実験していたのが不要になったので削除。

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -37,30 +37,6 @@ export default {
   }
 }
 
-// unicodeの位置が配列で出力される(10進数)
-// https://note.kiriukun.com/entry/20180925-charcodeat-vs-codepointat
-// Array(8)
-// 0: 65
-// 1: 66
-// 2: 12354
-// 3: 12356
-// 4: 127813
-// 5: 128036
-// 6: 33
-// 7: 127765
-// length: 8
-var chars = 'ABあい🍅🐤!🌕'.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]|[\s\S]/g) || []
-var points = []
-for (var i = 0; i < chars.length; ++i) {
-  points.push(chars[i].codePointAt(0))
-}
-console.log('codePointAt() => ', points)
-var decodeResult = []
-for (var j = 0; j < points.length; ++j) {
-  decodeResult.push(String.fromCodePoint(points[j]))
-}
-console.log(decodeResult)
-console.log(decodeResult.join(''))
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
