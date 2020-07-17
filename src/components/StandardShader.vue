099c712d93c0610b2f609ee6c43f9797fac56abc
whisky shusuky <whisky.shusuky@gmail.com>
2020-07-17 12:07:30 +0900

    feat(components): 試しにStandardShaderの中で配列を定義してUnicode位置を埋め込めるようにした。

--- src/components/StandardShader.vue
+++ src/components/StandardShader.vue
@@ -25,7 +25,7 @@ export default {
   methods: {
     downloadText: function () {
       var string = this.constructBody()
-      var title = 'testfile.txt'
+      var title = 'TestSurfaceShader.shader'
       var blobType = 'text/plain'
       var linkTagId = 'getLocal'
       var linkTag = document.getElementById(linkTagId)
@@ -42,10 +42,63 @@ export default {
     },
     constructBody: function () {
       return `
-head
-  unicode points are ${this.unicodePoints}!
-  unicode points number is ${this.unicodePoints.length}!
-last
+Shader "Custom/TestSurfaceShader"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+// Upgrade NOTE: excluded shader from DX11, OpenGL ES 2.0 because it uses unsized arrays
+#pragma exclude_renderers d3d11 gles
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+
+        // Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+        // See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+        // #pragma instancing_options assumeuniformscaling
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        static int array[${this.unicodePoints.length}] = {${this.unicodePoints}};
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            // Albedo comes from a texture tinted by color
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            o.Albedo = c.rgb;
+            // Metallic and smoothness come from slider variables
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
+
 `
     }
   }
