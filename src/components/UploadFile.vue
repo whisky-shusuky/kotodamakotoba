52446730893a10b8c1c67031455c601f39e1faf6
whisky shusuky <whisky.shusuky@gmail.com>
2020-07-21 05:24:58 +0900

    fix(components): テキストがドラッグドロップされたらコンソールに内容が流れるようにした。

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -1,18 +1,12 @@
 <template>
-  <form action="******" method="post" enctype="multipart/form-data">
   <div id="upload" class="form-group commonStyle" v-bind:class="{'styleA':styleA, 'styleB':styleB}" @dragover.prevent="changeStyle($event,'ok')" @dragleave.prevent="changeStyle($event,'no')" @drop.prevent="uploadFile($event)">
     <label for="upload_image" class="button">
-    <p>画像を選択</p>
+    <p>ファイルを選択</p>
     <input id="upload_image" type="file" name="img" @change="uploadFile($event)" style="display:none;" accept="image/*">
     </label>
-    <!-- ここからプレビュー機能の部分 -->
-    <p>またはここに画像ファイルをドラッグ＆ドロップ</p>
-    <img v-show="preview" v-bind:src="preview" style="width:300px;">
+    <p>またはここにファイルをドラッグ＆ドロップ</p>
     <p v-show="preview"> {{name}} </p>
-    <!-- ここまでプレビュー機能の部分 -->
   </div>
-    <button class="btn btn-success" style="width:400px; margin-left:30px;">アップロード</button>
-  </form>
 </template>
 
 <script>
@@ -34,8 +28,9 @@ export default {
       const reader = new FileReader()
       reader.onload = event => {
         this.preview = event.target.result
+        console.log(reader.result)
       }
-      reader.readAsDataURL(file)
+      reader.readAsText(file)
       this.name = files[0].name
       document.getElementById('upload_image').files = files
     },
