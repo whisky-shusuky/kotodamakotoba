f323ecfc8f0cf1ed4919040d7b758ec9f353601b
whisky shusuky <whisky.shusuky@gmail.com>
2020-07-21 13:11:52 +0900

    fix(components RotateBox): とりあえず任意のGLSLをくみこめることを確認

--- src/components/RotateBox.vue
+++ src/components/RotateBox.vue
@@ -13,7 +13,22 @@ export default {
     const camera = new THREE.PerspectiveCamera(75, 600 / 400, 0.1, 1000)
     const light = new THREE.DirectionalLight(0xffffff)
     const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshNormalMaterial()
+
+    var test = 1.0
+    var frag = `
+      uniform sampler2D map;
+      varying vec2 vUv;
+      void main() {
+  
+          gl_FragColor = vec4(0.5,${test},${test},1.0);
+      }`
+
+    const material = new THREE.ShaderMaterial({
+      fragmentShader: frag,
+      transparent: true,
+      depthWrite: true,
+      side: THREE.DoubleSide
+    })
     const cube = new THREE.Mesh(geometry, material)
     return { scene, renderer, camera, light, geometry, material, cube }
   },
