146e0e8d0458789a4499d39574d25e03e63f400d
whisky shusuky <whisky.shusuky@gmail.com>
2020-07-31 19:19:23 +0900

    feat(js: state): unicode位置と色を変える割合をvuexで状態管理するようにした。

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,29 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  message: ''
+  message: '',
+  unicodePoints: [],
+  rWeight: 0,
+  gWeight: 0,
+  bWeight: 0
 }
 
+// TODO: 対象state名を文字列で渡して汎用的に更新できるほうにした方がいい？（よく分かっていない）
 const mutations = {
   setMessage (state, val) {
     state.message = val
+  },
+  setUnicodePoints (state, val) {
+    state.unicodePoints = val
+  },
+  setRWeight (state, val) {
+    state.rWeight = val
+  },
+  setGWeight (state, val) {
+    state.gWeight = val
+  },
+  setBWeight (state, val) {
+    state.bWeight = val
   }
 }
 
